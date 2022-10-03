0b4a9ace32f480029068366e14784f3c3a70c295
Molel Levy <molzieyy@gmail.com>
2022-10-04 00:48:49 +0300

    Pluralsight learning skills

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.set('view engine','ejs');
 app.use('/', express.static(path.join(__dirname, 'public/')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index',{title:'Index'});
 });
 
 app.listen(3000, () => {
