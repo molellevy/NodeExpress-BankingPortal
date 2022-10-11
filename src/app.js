0ec58047d82f531193879a9428b8b446497b9ed5
Molel Levy <molzieyy@gmail.com>
2022-10-11 19:41:32 +0300

    Fix the app.js bug

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use('/', express.static(path.join(__dirname, 'public/')));
-app.use(express.urlencoded({extended:tru}))
+app.use(express.urlencoded({extended:true}))
 
 const accountData = fs.readFileSync('./src/json/accounts.json',{encoding:'UTF8'})
 
